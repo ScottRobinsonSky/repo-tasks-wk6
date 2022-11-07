6add84cafbfd995371f1171015556f621e0a9cf3
Scott Robinson <scott.robinson2@sky.uk>
2022-11-07 14:41:27 +0000

    Complete d1/2-medium tasks

--- d1/2-medium/shouty.js
+++ d1/2-medium/shouty.js
@@ -4,5 +4,11 @@ const app = express();
 
 // Add a GET handler that always responds successfully with the route in
 // uppercase, i.e., GET /hello should respond with the text body: HELLO!
+app.all("*", (req, resp) => {
+    if (req.method === "GET" && req.path != "/") {
+        resp.send(req.path.slice(1).toUpperCase() + "!");
+    }
+    resp.sendStatus(404);
+});
 
 module.exports = app;
--- d1/2-medium/web-server.js
+++ d1/2-medium/web-server.js
@@ -4,8 +4,10 @@ const path = require('path');
 const app = express();
 
 const publicFolder = path.join(__dirname, 'public');
+const assetsFolder = path.join(__dirname, "assets");
 
 // Serve static web pages from the `public` folder, and resources from the
 // `assets` folder.
+app.use("/", express.static(publicFolder), express.static(assetsFolder));
 
 module.exports = app;
