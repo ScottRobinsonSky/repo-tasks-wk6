9c42c4e294b9cfebae1e9ae0d49b231cc4a856c4
Scott Robinson <scott.robinson2@sky.uk>
2022-11-07 14:41:41 +0000

    Complete d1/3-hard task

--- d1/3-hard/app-server.js
+++ d1/3-hard/app-server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const app = express();
 
 // Serve static resources from the `assets` folder.
+const assetsFolder = path.join(__dirname, "assets");
+app.use("/", express.static(assetsFolder));
 
 // Add a GET handler that always responds successfully with the current time
 // in a web page, using the following template:
@@ -31,5 +33,25 @@ const app = express();
 // SS is current seconds
 //
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time
+
+app.get("/time", (req, resp) => {
+  const datetime = new Date()
+  const ISOString = datetime.toISOString();
+  const hours = datetime.getHours().toString().padStart(2, '0');
+  const minutes = datetime.getMinutes().toString().padStart(2, '0');
+  const seconds = datetime.getSeconds().toString().padStart(2, '0');
+  resp.send(`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <title>Time</title>
+      <link rel="stylesheet" href="/css/style.css" />
+      <script src="/js/script.js" defer></script>
+    </head>
+    <body>
+      <time datetime="${ISOString}">${hours}:${minutes}:${seconds}</time>
+    </body>
+  </html>`);
+});
 
 module.exports = app;
