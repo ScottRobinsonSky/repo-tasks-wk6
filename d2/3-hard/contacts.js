e7d226231076a2115f36ccad1cc93d78e2c7d395
Scott Robinson <scott.robinson2@sky.uk>
2022-11-08 15:34:24 +0000

    Complete d2/3-hard task

--- d2/3-hard/contacts.js
+++ d2/3-hard/contacts.js
@@ -7,6 +7,9 @@ app.use(express.json());
 
 // Add a GET handler on the "/contacts" route that returns the collection of
 // contacts.
+app.get("/contacts", async (req, resp) => {
+    resp.json(await Contact.findAll());  
+});
 
 // Add a POST handler on the "/contacts" route that expects a `firstName` and
 // `lastName` parameter, and an optional `emailAddress` parameter:
@@ -16,6 +19,18 @@ app.use(express.json());
 //
 // - if either the `firstName` or `lastName` are missing, respond with a Bad
 //   Request status code.
+app.post("/contacts", async (req, resp) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const emailAddress = req.body.emailAddress;
+
+    if (firstName === undefined || lastName === undefined) {
+        resp.sendStatus(400);
+        return;
+    }
+    await Contact.create({firstName: firstName, lastName: lastName, emailAddress: emailAddress});
+    resp.sendStatus(201);
+});
 
 // Add a GET handler on the "/contacts/:id" route:
 //
@@ -23,9 +38,21 @@ app.use(express.json());
 //   key), respond with the associated contact.
 //
 // - if the `id` is invalid, respond with a Not Found status code.
+app.get("/contacts/:id", async (req, resp) => {
+    const contact = await Contact.findByPk(+req.params.id);
+    if (contact === null) {
+        resp.sendStatus(404);
+        return;
+    }
+    resp.json(contact.toJSON());
+});
 
 // Add a DELETE handler on the "/contacts/:id" route which deletes the
 // associated contact (if it exists). Respond with an OK status code.
+app.delete("/contacts/:id", async (req, resp) => {
+    await Contact.destroy({where: {id: +req.params.id}});
+    resp.sendStatus(200);
+});
 
 // Add a PUT handler on the "/contacts/:id" route that expects a `firstName`
 // and `lastName` parameter, and an optional `emailAddress` parameter:
@@ -38,6 +65,28 @@ app.use(express.json());
 //
 // - if either the `firstName` or `lastName` are missing, respond with a Bad
 //   Request status code.
+app.put("/contacts/:id", async (req, resp) => {
+    const newFirstName = req.body.firstName;
+    const newLastName = req.body.lastName;
+    const newEmailAddress = req.body.emailAddress;
+
+    if (newFirstName === undefined || newLastName === undefined) {
+        resp.sendStatus(400);
+        return;
+    }
+
+    const paramsToChange = {
+        ...{firstName: newFirstName, lastName: newLastName},
+        ...(newEmailAddress === undefined ? {} : {emailAddress: newEmailAddress})
+    };
+
+    const [rowsUpdated] = await Contact.update(paramsToChange, {where: {id: +req.params.id}});
+    if (rowsUpdated === 0) {
+        resp.sendStatus(404);
+        return;
+    }
+    resp.sendStatus(200);
+});
 
 // Add a PATCH handler on the "/contacts/:id" route that optionally expects a
 // `firstName`, `lastName`, and `emailAddress` parameter:
@@ -47,5 +96,19 @@ app.use(express.json());
 // - if any of the parameters are provided, update the associated contact and
 //   respond with an OK status code.
 //
+app.patch("/contacts/:id", async (req, resp) => {
+    const paramsToChange = {
+        ...(req.body.firstName === undefined ? {} : {firstName: req.body.firstName}),
+        ...(req.body.lastName === undefined ? {} : {lastName: req.body.lastName}),
+        ...(req.body.emailAddress === undefined ? {} : {emailAddress: req.body.emailAddress})
+    };
+    
+    const [rowsUpdated] = await Contact.update(paramsToChange, {where: {id: +req.params.id}});
+    if (rowsUpdated === 0) {
+        resp.sendStatus(404);
+        return;
+    }
+    resp.sendStatus(200);
+});
 
 module.exports = app;
