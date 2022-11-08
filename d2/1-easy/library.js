370076a1003ac1065fffcd22d232a5d7dc5a1182
Scott Robinson <scott.robinson2@sky.uk>
2022-11-08 15:33:20 +0000

    Complete d2/1-easy task

--- d2/1-easy/library.js
+++ d2/1-easy/library.js
@@ -5,9 +5,21 @@ const app = express();
 
 // Add a GET handler on the "/books" route that responds with the collection of
 // books in an `application/json` response body.
+app.get("/books", (req, resp) => {
+    resp.json(books);
+});
 
 // Add a GET handler on the "/books/:isbn" route that responds with a single
 // book in an `application/json` response body. If the book is not found,
 // respond with the Not Found status.
+app.get("/books/:isbn", (req, resp) => {
+    const wantedIsbn = req.params.isbn;
+    for (let book of books) {
+        if (book.isbn === wantedIsbn) {
+            resp.json(book);
+        }
+    }
+    resp.sendStatus(404);
+});
 
 module.exports = app;
