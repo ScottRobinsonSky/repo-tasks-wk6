da8db908e3114db91474b93f6b5887cd8755012f
Scott Robinson <scott.robinson2@sky.uk>
2022-11-09 15:25:34 +0000

    Complete d3/2-medium task

--- d3/2-medium/login.js
+++ d3/2-medium/login.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const { Op } = require("sequelize");
 const { User } = require("./model/user");
 
 const app = express();
+app.use(express.json());
 
 // Add a POST handler on the "/login" route that expects a `username_or_email`
 // and a `password` parameter in a JSON body.
@@ -13,5 +15,31 @@ const app = express();
 //   mactches, send a redirect to "/" in response.
 //
 // - Otherwise, send an Unauthorized status in reponse.
+app.post("/login", async (req, resp) => {
+    const usernameOrEmail = req.body.username_or_email;
+    const password = req.body.password;
+    if (usernameOrEmail === undefined || password === undefined) {
+        resp.sendStatus(400);
+        return;
+    }
+    const user = await User.findOne({where: {
+        [Op.and]: [
+            {
+                [Op.or]: [
+                    {username: usernameOrEmail},
+                    {email: usernameOrEmail}
+                ],
+            },
+            {
+                password: password
+            }
+        ]
+    }});
+    if (user === null) {
+        resp.sendStatus(401);
+    } else {
+        resp.redirect("/");
+    }
+});
 
 module.exports = app;
