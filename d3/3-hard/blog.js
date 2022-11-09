35b055e33c2adf29a65647f04c4b4ae05a20d758
Scott Robinson <scott.robinson2@sky.uk>
2022-11-09 15:25:45 +0000

    Complete d3/3-hard task

--- d3/3-hard/blog.js
+++ d3/3-hard/blog.js
@@ -4,19 +4,49 @@ const { Category, Post } = require("./models");
 const app = express();
 
 // Add a GET handler on the "/blog" route that returns a collection of posts.
+app.get("/blog", async (req, resp) => {
+    resp.json(await Post.findAll());
+});
 
 // Add a GET handler on the "/blog/:slug" route that returns a single post with
 // a matching `slug`.
+app.get("/blog/:slug", async (req, resp) => {
+    const post = await Post.findOne({where: {slug: req.params.slug}});
+    if (post === null) {
+        resp.sendStatus(404);
+        return;   
+    }
+    resp.json(post.toJSON());
+})
 
 // Add a GET handler on the "/categories" route that returns a collection of
 // categories.
+app.get("/categories", async (req, resp) => {
+    resp.json(await Category.findAll());
+});
 
 // Add a GET handler on the "/categories/:slug" route that returns a single
 // category with a matching `slug`.
+app.get("/categories/:slug", async (req, resp) => {
+    const category = await Category.findOne({where: {slug: req.params.slug}});
+    if (category === null) {
+        resp.sendStatus(404);
+        return;
+    }
+    resp.json(category.toJSON());
+});
 
 // Add a GET handler on the "/categories/:slug/posts" route that returns a
 // a collection of posts that are in the matching category.
+app.get("/categories/:slug/posts", async (req, resp) => {
+    const category = await Category.findOne({where: {slug: req.params.slug}});
+    if (category === null) { 
+        resp.sendStatus(404);
+        return;
+    }
+    const posts = await category.getPosts();
+    resp.json(posts);
+});
 
-// Return JSON responses and appropriate statuses.
 
 module.exports = app;
